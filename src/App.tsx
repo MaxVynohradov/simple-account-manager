9d44f08779c05ec838060997f11849669a5a7095
Maksym Vynohradov <mv@madappgang.com>
2020-04-19 02:17:25 +0300

    feat: add general assets and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'react-jss';
+
+import * as theme from './theme';
 
 const App: React.FC = () => {
-  return <div className="App" />;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App" />
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,18 @@
+export default {
+  font: {
+    color: {
+      primary: '#000000',
+      secondary: '#9BB0CB',
+      regular: '#475666',
+      error: '#EB5757',
+      white: '#FFFFFF',
+    },
+  },
+  background: {
+    color: {
+      paper: '#E5E5E5',
+      primary: '#5E97F3',
+      activeTabBody: '#E7F0FF',
+    },
+  },
+};
